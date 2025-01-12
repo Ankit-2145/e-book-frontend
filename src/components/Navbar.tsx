11cf948890530611a71ac6a0c69b20327762c9e0
Ankit-2145 <ankitsharma2615@gmail.com>
2025-01-12 16:08:12 +0530

    add darkMode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,19 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "./toggle";
 
 export default function Navbar() {
   return (
-    <nav className="max-w-7xl mx-auto flex items-center justify-between p-4 bg-white shadow-sm">
+    <nav className="max-w-7xl mx-auto flex items-center justify-between p-4 bg-background shadow-sm">
       <div className="flex items-center">
         <Link href="/">
-          <span className="text-xl font-bold text-gray-800">E Books</span>
+          <span className="text-xl font-bold text-foreground">E Books</span>
         </Link>
       </div>
       <div className="flex items-center space-x-4">
+        <ModeToggle />
         <Button variant="ghost">Sign In</Button>
         <Button>Sign Up</Button>
       </div>
