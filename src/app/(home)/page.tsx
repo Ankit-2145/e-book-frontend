1c0996269f6fa4dbd3ebc20dcaeb074180772006
Ankit-2145 <ankitsharma2615@gmail.com>
2025-01-14 15:48:45 +0530

    optimized data fetching

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,14 @@
 import Banner from "@/app/(home)/components/Banner";
 import BookList from "./components/BookList";
+import { Suspense } from "react";
 
 export default async function Home() {
-  const response = await fetch(`${process.env.BACKEND_URL}/books`);
-  if (!response.ok) {
-    throw new Error("An error occurred while fetching the books");
-  }
-
-  const books = await response.json();
-
   return (
     <>
       <Banner />
-      <BookList books={books} />
+      <Suspense fallback={"Loading..."}>
+        <BookList />
+      </Suspense>
     </>
   );
 }
